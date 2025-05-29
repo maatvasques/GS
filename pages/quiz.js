7e3b1144319410e8e50f8f4c8400fcc7ec9e8a53
Vitor <vitoralcan5@gmail.com>
2025-05-29 15:57:41 -0300

    Retirei botão enviar

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,2 +1,4 @@
  document.getElementById('icone').style.filter = "brightness(0) invert(1)";
  document.getElementById('arrow__right').style.filter = "brightness(0) invert(1)";
+
+ const resposta = document.getElementById('quizForm');
